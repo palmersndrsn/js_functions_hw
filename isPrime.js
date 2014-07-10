d5fbd7b0a2254da8d114abb8b3ca7589837b9fae
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-10 15:21:12 -0700

    sending again/not great update

---
+++ isPrime.js
@@ -0,0 +1,10 @@
+var isPrime = function (num) {
+	for (i = 0; i <= 9; i++){
+	if (num  % (i) === 0) {
+	return true 
+    }
+  }
+}
+console.log(isPrime(10))
+
+//not working or done 
--- numSquare.js
+++ numSquare.js
@@ -1,10 +1,10 @@
 var myArray = []
 var numSquare = function (maxNum) {
 		for (var i = 0; i <= maxNum; i += 1) {
-			var sqr = Math.sqrt(i)
+		// var sqr = Math.sqrt(i)
 		// if (i === (Math.sqrt(i) * Math.floor(i))) ; { attempt 9
-		if (Math.floor(sqr) * i === i ) ; {     //attempt 10 
-
+		// if (Math.floor(sqr) * i === i ) ; {     //attempt 10 
+		if (Math.sqrt(i) % 1 === 0) ; { 		//attempt 11
 		// if (Math.sqrt(i).toString()[1] !== '.') { //correct answer
 			myArray.push(i);
 		}
