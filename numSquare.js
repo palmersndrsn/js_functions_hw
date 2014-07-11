79ef99c661c33af4aa71ea35bacb4c100ef84347
DoriPS13 <palmer.sanderson@gmail.com>
2014-07-10 17:50:11 -0700

    I fixed itgit add -A

--- numSquare.js
+++ numSquare.js
@@ -1,16 +1,12 @@
 var myArray = []
 var numSquare = function (maxNum) {
 		for (var i = 0; i <= maxNum; i += 1) {
-		// var sqr = Math.sqrt(i)
-		// if (i === (Math.sqrt(i) * Math.floor(i))) ; { attempt 9
-		// if (Math.floor(sqr) * i === i ) ; {     //attempt 10 
-		if (Math.sqrt(i) % 1 === 0) ; { 		//attempt 11
-		// if (Math.sqrt(i).toString()[1] !== '.') { //correct answer
+		
+		if (Math.sqrt(i) % 1 === 0) { 		
+		
 			myArray.push(i);
 		}
 	}
 	return myArray;
 } 
 console.log(numSquare(20));
-
-//with mine every number 1-20 appears in the array
